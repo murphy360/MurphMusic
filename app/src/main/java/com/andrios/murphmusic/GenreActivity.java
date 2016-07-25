629089bada2a2e3244416ab0e63b6cdbd9eb93a8
Corey Murphy <c.murphy360@gmail.com>
2016-07-24 20:13:24 -0700

    Started Design on Genre Detail Screen.

---
+++ app/src/main/java/com/andrios/murphmusic/GenreActivity.java
@@ -0,0 +1,13 @@
+package com.andrios.murphmusic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GenreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_genre);
+    }
+}
